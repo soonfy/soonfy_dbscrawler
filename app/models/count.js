f29af3201c24354fa1eccd71235a4d651352a0a2
soonfy <soonfy@163.com>
2016-07-25 17:40:01 +0800

    对应豆瓣数据库，采集并汇总视频网站数据

--- app/models/count.js
+++ app/models/count.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose')
 var CountSchema = require('../schemas/count')
-var Count = mongoose.model('album', CountSchema)
+var Count = mongoose.model('vs_counts', CountSchema)
 
 module.exports = Count
